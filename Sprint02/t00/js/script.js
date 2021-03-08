bff666d80fc1d4c0204cd6f1f18390cdb54410f7
Oleg <73128022+NogaKazaha@users.noreply.github.com>
2021-03-08 11:38:28 +0200

    t00?

---
+++ Sprint02/t00/js/script.js
@@ -0,0 +1,4 @@
+var message = 'Hello JavaScript from outside!';
+alert(message);
+// An alert box is often used if you want to make sure information comes through to the user. 
+// When an alert box pops up, the user will have to click "OK" to proceed.
