75375d4888e7f25f4b4ca76b93b8a7b8cea3d8ce
Oleg <73128022+NogaKazaha@users.noreply.github.com>
2021-03-16 13:35:20 +0200

    some changes 05

--- Sprint04/t05/js/script.js
+++ Sprint04/t05/js/script.js
@@ -1,38 +1,36 @@
-let stonessArea = document.getElementById('container');
-let state = {
-  offsetX: 0,
-  offsetY: 0,
-  target: null
-};
-
-stonessArea.addEventListener('dblclick', event => {
-  const target = event.target;
-
-  if (target && target.classList.contains('stones')) {
-    if (target.getAttribute('value') === 'on') {
-      target.setAttribute('value', 'off');
-    } else {
-      target.setAttribute('value', 'on');
+function moveStones() {
+  let container = document.getElementById('container');
+  let state = {
+    offsetX: 0,
+    offsetY: 0,
+    target: null
+  };
+  container.addEventListener('on_click', event => {
+    const new_target = event.target;
+    if (new_target && new_target.classList.contains('stones')) {
+      if (new_target.getAttribute('value') === 'on') {
+        new_target.setAttribute('value', 'off');
+      } else {
+        new_target.setAttribute('value', 'on');
+      }
     }
-  }
-});
+  });
+  container.addEventListener('mousedown', event => {
+    if (event.target && event.target.classList.contains('stones') && event.target.getAttribute('value') === 'on') {
+      state.target = event.target;
+      state.offsetX = event.offsetX; 
+      state.offsetY = event.offsetY;
+    }
+  });
+  container.addEventListener('mouseup', () => {
+    state.target = null;
+  });
+  container.addEventListener('mousemove', e => {
+    if (state.target) {
+      state.target.style.left = (e.pageX - state.offsetX) + 'px';
+      state.target.style.top = (e.pageY - state.offsetY) + 'px';
+    }
+  });
+}
 
-stonessArea.addEventListener('mousedown', event => {
-  if (event.target && event.target.classList.contains('stones') &&
-      event.target.getAttribute('value') === 'on') {
-    state.target = event.target;
-    state.offsetX = event.offsetX; 
-    state.offsetY = event.offsetY;
-  }
-});
-
-stonessArea.addEventListener('mouseup', () => {
-  state.target = null;
-});
-
-stonessArea.addEventListener('mousemove', e => {
-  if (state.target) {
-    state.target.style.left = (e.pageX - state.offsetX) + 'px';
-    state.target.style.top = (e.pageY - state.offsetY) + 'px';
-  }
-});
+moveStones();
