4dd4ae2e8ccff56bac193400d8f58c37b42bf63a
Oleh Savich <osavich@c1r5p3.khpi.ucode-connect.study>
2021-03-23 13:13:38 +0200

    t03

--- Sprint05/t03/index.php
+++ Sprint05/t03/index.php
@@ -1,3 +1,20 @@
 <?php
-  
+  function firstUpper($str) {
+    if (!$str || $str == NULL) {
+      return "";
+    }
+    $str = trim($str, " \n\r\t\v\0");
+    $str = ucfirst(strtolower($str));
+    return $str;
+  }
+
+  echo ('"testing String" : ' . firstUpper("testing String")) . "\n";
+  echo ('"   testing   String" : ' . firstUpper("   testing   String")) . "\n";
+  echo ('"07" : ' . firstUpper("07")) . "\n";
+  echo ('"" : ') . firstUpper("") . "\n";
+  echo ('true : ' . firstUpper(true)) . "\n";
+  echo ('NULL : ' . firstUpper(NULL)) . "\n";
+
+  echo (firstUpper("   ...I Will Rebuild Krypton Atop His Bones.")) . "\n";
+  echo (firstUpper(" 300room FOR yoUr  DESTiny   ")) . "\n";
 ?>
