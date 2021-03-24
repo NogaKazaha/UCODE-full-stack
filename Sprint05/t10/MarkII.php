796374ab1e48b76f6eb4e9184e62921587403559
Oleg <73128022+NogaKazaha@users.noreply.github.com>
2021-03-24 13:03:10 +0200

    t10

---
+++ Sprint05/t10/MarkII.php
@@ -0,0 +1,19 @@
+<?php
+  class MarkII {
+    public function makeBoom() {
+      $arr = "2 x Repulsors";
+      return $arr;
+    }
+  }
+  // spl_autoload_register(function($class_name) {
+  //   include $class_name. '.php';
+  // });
+
+  // $mark_II = new MarkII();
+  // echo $mark_II->makeBoom() ."\n";
+  // class WarMachine extends MarkII {
+  //     use Update;
+  // }
+  // $wm = new WarMachine();
+  // print_r($wm->makeBoom());
+?>
