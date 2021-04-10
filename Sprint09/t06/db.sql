544da8ba69ba78449c1a69494bbc3576551b0657
Oleg <73128022+NogaKazaha@users.noreply.github.com>
2021-04-10 20:23:08 +0300

    t06

---
+++ Sprint09/t06/db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS sword;
+CREATE USER IF NOT EXISTS osavich@localhost IDENTIFIED WITH mysql_native_password BY 'securepass';
+GRANT ALL PRIVILEGES ON sword.* TO osavich@localhost WITH GRANT OPTION;
+USE sword;
+CREATE TABLE IF NOT EXISTS sword.users (
+  id INT NOT NULL AUTO_INCREMENT , 
+  login VARCHAR(30) NOT NULL , 
+  password VARCHAR(100) NOT NULL , 
+  full_name VARCHAR(30) NOT NULL , 
+  email VARCHAR(50) NOT NULL , 
+  admin ENUM('0', '1') NOT NULL DEFAULT '0' ,
+  PRIMARY KEY (id),
+  UNIQUE (login),
+  UNIQUE (email) 
+) ENGINE = InnoDB;
